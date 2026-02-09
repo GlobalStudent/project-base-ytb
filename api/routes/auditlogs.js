5c9377062f60509db45aba3f8d4ff2e247e54787
Zafer Yılmaz <yilmazzafer9@gmail.com>
2026-02-09 23:40:12 +0300

    #1

--- api/routes/auditlogs.js
+++ api/routes/auditlogs.js
@@ -1,15 +1,45 @@
 const express = require("express");
+const moment = require("moment");
+const Response = require("../lib/Response");
+const AuditLogs = require("../db/models/AuditLogs");
 const router = express.Router();
 
+router.post("/", async (req, res) => {
+    try {
 
-router.get("/:id", (req, res, next) => {
-    res.json({
-        body: req.body,
-        params: req.params,
-        query: req.query,
-        headers: req.headers
-    });
+        let body = req.body;
+        let query = {};
+        let skip = body.skip;
+        let limit = body.limit;
+
+        if (typeof body.skip !== "number") {
+            skip = 0;
+        }
+
+        if (typeof body.limit !== "number" || body.limit > 500) {
+            limit = 500;
+        }
+
+        if (body.begin_date && body.end_date) {
+            query.created_at = {
+                $gte: moment(body.begin_date),
+                $lte: moment(body.end_date)
+            }
+        } else {
+            query.created_at = {
+                $gte: moment().subtract(1, "day").startOf("day"),
+                $lte: moment()
+            }
+        }
+
+        let auditLogs = await AuditLogs.find(query).limit(limit).skip(skip).sort({ created_at: -1 });
+
+        res.json(Response.successResponse(auditLogs));
+
+    } catch (err) {
+        let errorResponse = Response.errorResponse(err);
+        res.status(errorResponse.code).json(errorResponse);
+    }
 });
-
 
 module.exports = router;
